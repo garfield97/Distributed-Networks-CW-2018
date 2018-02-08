fad921b084f8ee572cc295faed43e5005d20172a
Edward Scaife <ejs16@point25.doc.ic.ac.uk>
2018-02-08 12:50:46 +0000

    Server now starts

--- rmi/RMIServer.java
+++ rmi/RMIServer.java
@@ -23,12 +23,12 @@ public class RMIServer extends UnicastRemoteObject implements RMIServerI {
 	public void receiveMessage(MessageInfo msg) throws RemoteException {
 
 		// TO-DO: On receipt of first message, initialise the receive buffer
-		int[] buff = new int[msg.totalMessages]; 
+		int[] buff = new int[msg.totalMessages];
 		// TO-DO: Log receipt of the message
-		System.out.println(msg.toString()+"recieved");
+
 		// TO-DO: If this is the last expected message, then identify
 		//        any missing messages
-		
+
 	}
 
 
@@ -44,9 +44,7 @@ public class RMIServer extends UnicastRemoteObject implements RMIServerI {
 		try {
 			RMIServer server = new RMIServer();
 		// TO-DO: Bind to RMI registry
-			Registry registry = LocateRegistry.getRegistry(1099);
-			registry.bind("Server",server);
-			System.out.println("Server Interface bound");
+			rebindServer("rmi://localhost/RMIServer",server);
 		}
 		catch(Exception e) {
 			System.err.println("Server Exception: " + e.toString());
@@ -59,14 +57,13 @@ public class RMIServer extends UnicastRemoteObject implements RMIServerI {
 		// TO-DO:
 		// Start / find the registry (hint use LocateRegistry.createRegistry(...)
 		// If we *know* the registry is running we could skip this (eg run rmiregistry in the start script)
-			Registry foundreg = LocateRegistry.createRegistry(1099/*port goes here*/);
-			
+			Registry foundreg = LocateRegistry.createRegistry(1099);
 		// TO-DO:
 		// Now rebind the server to the registry (rebind replaces any existing servers bound to the serverURL)
 		// Note - Registry.rebind (as returned by createRegistry / getRegistry) does something similar but
 		// expects different things from the URL field.
-			foundreg.rebind("server",new RMIServer());
-			System.out.println("Game Time Started");
+			foundreg.rebind(serverURL,server);
+			System.out.println("server started");
 		} catch(RemoteException e){
 				System.err.println("Server Exception: " + e.toString());
 				e.printStackTrace();
